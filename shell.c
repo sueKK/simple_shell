64fea83ad427406dfe6532f577afc02ec9e2334e
lewisjn-arch <lhinga75@gmail.com>
2023-10-18 20:03:36 +0000

    solve task 1 functionality

--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
  * main - Entry point of the program
  *
  * Description: This is a simple shell project implementation
- * with minimal functionality.
+ * 
  *
  * Return: Always returns 0.
  */
@@ -13,29 +13,21 @@ int main(void)
 	char input[MAX_INPUT_SIZE];
 	char *prompt = "($) ";
 	char **command_and_args;
-	char *new_path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
-
-	if (setenv("PATH", new_path, 1) == -1)
-	{
-		perror("setenv");
-		return (1);
-	}
 
 	while (1)
 	{
 		write_string(STDOUT_FILENO, prompt);
 
-		if (fgets(input, sizeof(input), stdin) == NULL)
+		if (fgets(input, _strlen(input), stdin) == NULL)
 		{
 			write_string(STDOUT_FILENO, "\n");
-			break;
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			exit(EXIT_SUCCESS);
 		}
 		input[_strlen(input) - 1] = '\0';
-
-		if (_strlen(input) == 0)
-		{
-			continue;
-		}
 		command_and_args = tokenize_input(input);
 
 		if (command_and_args != NULL)
