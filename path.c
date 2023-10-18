44d04eb5f2e09a53d5114ce8066e05968e45cb59
lewisjn-arch <lhinga75@gmail.com>
2023-10-18 21:57:14 +0000

    fix recursive function

--- path.c
+++ path.c
@@ -87,7 +87,7 @@ char **get_environment_variables()
  */
 char **retrieve_environment_variables()
 {
-	char **environ;
+	char **environ = retrieve_environment_variables();
 	int env_count = 0;
 	int i;
 	char **var = environ;
